cba3e91311bd653c341e3f11afcdd2ee5c64af39
Tamilselvan.M <sktamilvb@gmail.com>
2026-09-19 16:37:20 +0530

    Update outputs.tf

--- ShopSphere - Microservices Application/terraform/outputs.tf
+++ ShopSphere - Microservices Application/terraform/outputs.tf
@@ -1,40 +1,28 @@
-output "eks_cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = aws_eks_cluster.shopsphere.name
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
-output "eks_cluster_endpoint" {
-  description = "Endpoint of the EKS cluster API server"
-  value       = aws_eks_cluster.shopsphere.endpoint
+output "cluster_endpoint" {
+  description = "EKS API endpoint"
+  value       = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_certificate_authority" {
-  description = "Base64-encoded certificate authority data"
-  value       = aws_eks_cluster.shopsphere.certificate_authority[0].data
-  sensitive   = true
+output "cluster_certificate_authority_data" {
+  value     = module.eks.cluster_certificate_authority_data
+  sensitive = true
 }
 
-output "vpc_id" {
-  description = "ID of the ShopSphere VPC"
-  value       = aws_vpc.shopsphere.id
-}
-
-output "public_subnet_ids" {
-  description = "Public subnet IDs for internet-facing load balancers"
-  value       = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-}
-
-output "private_subnet_ids" {
-  description = "Private subnet IDs used by EKS worker nodes"
-  value       = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+output "region" {
+  value = var.aws_region
 }
 
 output "ecr_repository_urls" {
-  description = "ECR repository URLs for ShopSphere services"
-  value       = { for name, repo in aws_ecr_repository.shopsphere : name => repo.repository_url }
+  description = "Map of service -> ECR URL"
+  value       = { for k, r in aws_ecr_repository.svc : k => r.repository_url }
 }
 
-output "kubeconfig_command" {
-  description = "Command to configure kubectl for the EKS cluster"
-  value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.shopsphere.name} --region ${var.aws_region}"
+output "configure_kubectl" {
+  description = "Run this to update kubeconfig"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
