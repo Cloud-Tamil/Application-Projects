153204937419ac5682a0dd0c1c426be8d87c2759
Tamilselvan.M <sktamilvb@gmail.com>
2026-09-19 16:36:57 +0530

    Update variables.tf

--- ShopSphere - Microservices Application/terraform/variables.tf
+++ ShopSphere - Microservices Application/terraform/variables.tf
@@ -1,47 +1,43 @@
 variable "aws_region" {
-  description = "AWS region to deploy into"
+  description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "EKS cluster name"
   type        = string
-  default     = "shopsphere-eks"
+  default     = "shopsphere-cluster"
 }
 
-variable "kubernetes_version" {
-  description = "Kubernetes version for EKS"
+variable "cluster_version" {
+  description = "Kubernetes version"
   type        = string
-  default     = "1.34"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  default     = "1.29"
 }
 
 variable "node_instance_type" {
-  description = "EC2 instance type for EKS worker nodes"
+  description = "EC2 instance type for worker nodes"
   type        = string
   default     = "t3.medium"
 }
 
 variable "node_desired_size" {
-  description = "Desired number of worker nodes"
-  type        = number
-  default     = 2
+  type    = number
+  default = 3
 }
 
 variable "node_min_size" {
-  description = "Minimum number of worker nodes"
-  type        = number
-  default     = 1
+  type    = number
+  default = 2
 }
 
 variable "node_max_size" {
-  description = "Maximum number of worker nodes"
-  type        = number
-  default     = 4
+  type    = number
+  default = 6
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
 }
